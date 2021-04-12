ee27b5daf59aed6dd7e49c091c093a15bd4d5532
unknown <c_hoang2@fanshaweonline.ca>
2021-04-12 01:45:46 -0400

    add video and hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 console.log("JavaScript is linked up");
+
+(function(){
+	"use strict";
+	console.log("fired");
+
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burgerCon");
+
+	function hamburgerMenu(){
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	button.addEventListener("click", hamburgerMenu);
+})();
